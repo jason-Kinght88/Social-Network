6905c658e61946caf30b40359af183a9aabd96fd
vocal-studios <jasonknight551@gmail.com>
2017-08-09 15:29:25 +0800

    FB and firebase login intergration
    login using firebase and facebook working

--- Social Network/signInVC.swift
+++ Social Network/signInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class signInVC: UIViewController {
 
@@ -20,6 +23,41 @@ class signInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func fbBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            
+            if error != nil {
+                
+                print("JASON: Unable to authenticate with Facebook - \(String(describing: error))")
+                
+            } else if result?.isCancelled == true {
+                
+                print("JASON: User cancelled authentication")
+                
+            } else {
+                
+                print("JASON: Successfully authenticated with Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential) {
+        
+        Auth.auth().signIn(with: credential) { (user, error) in
+            
+            if error != nil {
+                print("JASON: Unable to authenticate with firebase - \(String(describing: error))")
+                
+            } else {
+                print("Succesfully authenticated with firebase")
+            }
+        }
+    }
 
 }
 
