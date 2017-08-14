e5bfd152df38309120a41170a01c6b1954f9bc9e
vocal-studios <jasonknight551@gmail.com>
2017-08-14 10:18:03 +0800

    Tableview and ui complete
    UI complete need to setup databases

---
+++ Social Network/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  Social Network
+//
+//  Created by Amy on 14/8/17.
+//  Copyright © 2017 Jason Knight. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+}
